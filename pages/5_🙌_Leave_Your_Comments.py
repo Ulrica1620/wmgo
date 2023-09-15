092703f8ebebc2922534b8601c913e27e96f789e
Ulrica1620 <123502584+Ulrica1620@users.noreply.github.com>
2023-09-15 12:26:05 -0700

    Update 5_🙌_Leave_Your_Comments.py

--- pages/5_🙌_Leave_Your_Comments.py
+++ pages/5_🙌_Leave_Your_Comments.py
@@ -1,12 +1,79 @@
 from datetime import datetime
-import streamlit as st
-import os
-dir_path = os.path.split(os.path.realpath(__file__))[0]
-import sys
-sys.path.append(dir_path)
+import socket
 
-import blind_watermark
-from blind_watermark import db
+import google_auth_httplib2
+import httplib2
+import pandas as pd
+import streamlit as st
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import HttpRequest
+
+socket.setdefaulttimeout(15 * 60)
+
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "1RtSCrn232rwYZ_DZAOU1lqzEha-2zP0ZjMf7342YZBA" #"1rkMVLvh3JrBq_tbi4Ho0qjCDAP3vYdNuWOEjYpkJLNU"
+SHEET_NAME = "Database"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+
+
+@st.cache_resource()
+def connect():
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[SCOPE],
+    )
+
+    # Create a new Http() object for every request
+    def build_request(http, *args, **kwargs):
+        new_http = google_auth_httplib2.AuthorizedHttp(
+            credentials, http=httplib2.Http()
+        )
+        return HttpRequest(new_http, *args, **kwargs)
+
+    authorized_http = google_auth_httplib2.AuthorizedHttp(
+        credentials, http=httplib2.Http()
+    )
+    service = build(
+        "sheets",
+        "v4",
+        requestBuilder=build_request,
+        http=authorized_http,
+    )
+    gsheet_connector = service.spreadsheets()
+    return gsheet_connector
+
+
+def collect(gsheet_connector) -> pd.DataFrame:
+    values = (
+        gsheet_connector.values()
+        .get(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A:C",
+        )
+        .execute()
+    )
+
+    df = pd.DataFrame(values["values"])
+    df.columns = df.iloc[0]
+    df = df[1:]
+    return df
+
+
+def insert(gsheet_connector, row) -> None:
+    values = (
+        gsheet_connector.values()
+        .append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A:C",
+            body=dict(values=row),
+            valueInputOption="USER_ENTERED",
+        )
+        .execute()
+    )
+
+
 st.set_page_config(
     page_title="Leave Your Comments",
     page_icon=":Raising Hands:",
@@ -21,8 +88,8 @@ COMMENT_TEMPLATE_MD = """{} - {}
 
 # Comments part
 
-conn = db.connect()
-comments = db.collect(conn)
+conn = connect()
+comments = collect(conn)
 
 with st.expander("💬 Open comments"):
 
@@ -52,7 +119,7 @@ with st.expander("💬 Open comments"):
 
     if submit:
         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
+        insert(conn, [[name, comment, date]])
         if "just_posted" not in st.session_state:
             st.session_state["just_posted"] = True
         st.experimental_rerun()
